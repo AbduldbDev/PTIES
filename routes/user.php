778de2d1bf92d7147d6b8511a5ccc28b23700bbe
Ethan Young <youngmeister9@gmail.com>
2025-08-15 14:20:48 +0800

    feat: Officials Biography page UI

--- routes/user.php
+++ routes/user.php
@@ -9,5 +9,6 @@ Route::get('/', [PageController::class, 'Home'])->name('user.home');
 Route::get('/about', [PageController::class, 'About'])->name('user.about');
 Route::get('/tourism', [PageController::class, 'AboutTourism'])->name('user.tourism');
 Route::get('/officials', [PageController::class, 'KeyOfficials'])->name('user.officials');
+Route::get('/biography', [PageController::class, 'OfficialBio'])->name('user.biography');
 
 Route::middleware('guest')->group(function () {});
