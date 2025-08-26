177f04317466932f340848f1e018ded4c85da97f
Abdul De Borja <abduldbdev@gmail.com>
2025-08-27 00:16:03 +0800

    feat: remove login backend validaiton

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -15,7 +15,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email'    => ['required', 'string', 'email', 'max:255'],
-            'password' => ['required', 'string', 'min:8', 'max:64'],
+            'password' => ['required', 'string',  'max:64'],
         ];
     }
 
