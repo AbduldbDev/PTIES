1d71bc2f7a246860f1e89193a5712cd308917ac4
Abdul Aziz <abdulaziz@MJ-2.local>
2025-09-25 03:32:18 +0800

    feat: Officials content

--- database/seeders/OfficialsSeeder.php
+++ database/seeders/OfficialsSeeder.php
@@ -13,29 +13,29 @@ class OfficialsSeeder extends Seeder
     public function run(): void
     {
         $officials = [
-            ['name' => 'Ronald James V. Hidalgo', 'position' => 'Municipal Mayor'],
-            ['name' => 'Ariel V. Formoles', 'position' => 'Municipal Vice Mayor'],
+            ['name' => 'Ronald James V. Hidalgo', 'position' => 'Municipal Mayor',  'image' => '/default/officials/Ronald Hidalgo.jpg'],
+            ['name' => 'Ariel V. Formoles', 'position' => 'Municipal Vice Mayor', 'image' => '/default/officials/Ariel Formoles.jpg'],
 
-            ['name' => 'Aileen S. Cailles', 'position' => 'SB Member'],
-            ['name' => 'Vlacheslav Vaarniv D. Martinez', 'position' => 'SB Member'],
-            ['name' => 'Amelita I. Del Moro', 'position' => 'SB Member'],
-            ['name' => 'Narcing A. Catarino', 'position' => 'SB Member'],
-            ['name' => 'Carville M. Auditor', 'position' => 'SB Member'],
-            ['name' => 'Elmer B. Cotoner', 'position' => 'SB Member'],
-            ['name' => 'Kevin M. Reyes', 'position' => 'SB Member'],
-            ['name' => 'Mark Benito R. Caballero', 'position' => 'SB Member'],
+            ['name' => 'Aileen S. Cailles', 'position' => 'SB Member', 'image' => '/default/officials/Aileen Cailles.jpg'],
+            ['name' => 'Vlacheslav Vaarniv D. Martinez', 'position' => 'SB Member', 'image' => '/default/officials/VV Martinez.jpg'],
+            ['name' => 'Amelita I. Del Moro', 'position' => 'SB Member', 'image' => '/default/officials/Amelita Del Moro.png'],
+            ['name' => 'Narcing A. Catarino', 'position' => 'SB Member', 'image' => '/default/officials/Narcing Catarino.jpg'],
+            ['name' => 'Carville M. Auditor', 'position' => 'SB Member', 'image' => '/default/officials/Carville Auditor.jpg'],
+            ['name' => 'Elmer B. Cotoner', 'position' => 'SB Member', 'image' => '/default/officials/Elmer Cotoner.jpg'],
+            ['name' => 'Kevin M. Reyes', 'position' => 'SB Member', 'image' => '/default/officials/Kevin Reyes.jpg'],
+            ['name' => 'Mark Benito R. Caballero', 'position' => 'SB Member', 'image' => '/default/officials/Mark Benito Caballero.jpg'],
 
-            ['name' => 'Elyne Suzan L. Eleda', 'position' => 'Municipal Accountant'],
-            ['name' => 'Armel S. Yerro', 'position' => 'Municipal Treasurer'],
-            ['name' => 'Cecilia SG. Balita', 'position' => 'Municipal Budget Officer'],
+            ['name' => 'Elyne Suzan L. Eleda', 'position' => 'Municipal Accountant', 'image' => '/default/officials/Elyne Suzan Eleda.jpg'],
+            ['name' => 'Armel S. Yerro', 'position' => 'Municipal Treasurer', 'image' => null],
+            ['name' => 'Cecilia SG. Balita', 'position' => 'Municipal Budget Officer', 'image' => '/default/officials/Cecilia SG Balita.jpg'],
         ];
 
         foreach ($officials as $official) {
             DB::table('officials')->insert([
                 'name' => $official['name'],
                 'position' => $official['position'],
+                'image' => $official['image'],
                 'term' => '2022-2025',
-                'image' => '/User/Images/ace.png',
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
--- resources/js/User/Utils/components/Cards/Officials.tsx
+++ resources/js/User/Utils/components/Cards/Officials.tsx
@@ -12,13 +12,14 @@ interface Props {
 export default function Officials({ official }: Props) {
     return (
         <div className="official-card flex flex-col overflow-hidden rounded-xl border border-gray-100 bg-white shadow-md transition-all duration-300 hover:shadow-lg">
-            <div className="relative aspect-square bg-gray-200">
+            <div className="relative aspect-square w-full overflow-hidden rounded-lg">
                 <img src={official?.image || '/images/user/User.png'} alt={official?.name || 'Official'} className="h-full w-full object-cover" />
 
                 <div className="absolute right-0 bottom-0 left-0 bg-gradient-to-t from-primary/50 to-transparent p-4">
                     <span className="rounded-full bg-primary px-2 py-1 text-xs text-white">{official.position}</span>
                 </div>
             </div>
+
             <div className="flex flex-grow flex-col p-5">
                 <div className="flex-grow">
                     <h4 className="text-dark mb-1 text-lg font-bold">{official.name}</h4>
